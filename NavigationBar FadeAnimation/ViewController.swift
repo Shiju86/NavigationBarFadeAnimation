0f0052d52b77f1682baf72ab6b118669dd76de12
Shiju Varghese <jaspreet@Shijus-MacBook-Pro.local>
2018-03-18 16:31:12 +0530

    NavigationBar Fade Animation when scroll the collectionView

--- NavigationBar FadeAnimation/ViewController.swift
+++ NavigationBar FadeAnimation/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  @IBOutlet weak var collectionView: UICollectionView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    collectionView.delegate = self
+    collectionView.dataSource = self
+    
+    self.navigationController?.navigationBar.tintColor = .white
+    self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+    self.navigationController?.navigationBar.shadowImage = UIImage()
+   
   }
 
   override func didReceiveMemoryWarning() {
@@ -21,5 +30,53 @@ class ViewController: UIViewController {
   }
 
 
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 9
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomCell", for: indexPath)
+    
+    
+    return cell
+    
+  }
+  
+  func collectionview(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndex: IndexPath) -> CGSize {
+    
+    return CGSize(width: collectionView.frame.size.width, height: 300)
+    
+  }
+  
+  func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    
+    var offSet = scrollView.contentOffset.y / 150
+    
+    let updateNavColor = UIColor(red: 1, green: 1, blue: 1, alpha: offSet)
+    
+    if offSet > 1 {
+      offSet = 1
+      //Update Navigation Item Color
+      self.navigationController?.navigationBar.tintColor = .red
+      
+    }else {
+      
+      self.navigationController?.navigationBar.tintColor = .white
+      
+    }
+    
+    //Update NavigationBar Background Color
+    self.navigationController?.navigationBar.backgroundColor = updateNavColor
+    
+    UIApplication.shared.statusBarView?.backgroundColor = updateNavColor
+    
+    
+  }
+  
 }
 
